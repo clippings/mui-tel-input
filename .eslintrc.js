a51697843545974508ebd66cc8a8f1f653b6eb73
Martin Stankov <mstankov4@gmail.com>
2023-08-30 00:47:47 +0300

    Removed docs to check if issue with tsconfig will be fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
   extends: [
-    '@viclafouch/eslint-config-viclafouch',
-    '@viclafouch/eslint-config-viclafouch/react',
-    '@viclafouch/eslint-config-viclafouch/hooks',
-    '@viclafouch/eslint-config-viclafouch/typescript',
-    '@viclafouch/eslint-config-viclafouch/prettier'
+    "@viclafouch/eslint-config-viclafouch",
+    "@viclafouch/eslint-config-viclafouch/react",
+    "@viclafouch/eslint-config-viclafouch/hooks",
+    "@viclafouch/eslint-config-viclafouch/typescript",
+    "@viclafouch/eslint-config-viclafouch/prettier",
   ],
   parserOptions: {
-    project: ['./tsconfig.json', './docs/tsconfig.json']
+    project: ["./tsconfig.json"],
   },
   rules: {
     // inputProps / InputProps
-    'react/jsx-no-duplicate-props': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
+    "react/jsx-no-duplicate-props": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
       {
         devDependencies: [
-          '.storybook/**',
-          'stories/**',
-          '**/*.stories.tsx',
-          '**/*.test.ts',
-          '**/*.test.tsx'
-        ]
-      }
-    ]
-  }
-}
+          ".storybook/**",
+          "stories/**",
+          "**/*.stories.tsx",
+          "**/*.test.ts",
+          "**/*.test.tsx",
+        ],
+      },
+    ],
+  },
+};
